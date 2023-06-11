4f489bb9819d74b07f02036702c06b43be116aae
Lukas Bergdoll <lukas.bergdoll@gmail.com>
2023-06-11 15:45:58 +0200

    Use specific rustdoc instead of linking README

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,15 @@
-#![doc = include_str!("../README.md")]
+//! The tiny_sort crate provides two sort implementations [`stable::sort`] and [`unstable::sort`].
+//! The crate is `no_std` and both versions can be disabled via features, by setting
+//! `default-features = false`. [`stable::sort`] requires `alloc`, [`unstable::sort`] doesn't. In
+//! addition to the default sort interface that requires that `T` implements [`Ord`],
+//! [`stable::sort_by`] [`unstable::sort_by`] can be used to sort with a custom comparison function.
+//!
+//! Use these sort implementations if you care about binary-size more than you care about
+//! performance. Otherwise use `slice::sort` and `slice::sort_unstable`.
+//!
+//! See the [README](https://github.com/Voultapher/tiny-sort-rs) for information about binary-size
+//! and run-time.
+
 #![no_std]
 
 #[cfg(feature = "stable")]
